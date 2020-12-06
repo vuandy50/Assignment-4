3d385834c0a10606aabc88c90bc783c2f72f1a6c
Andy <49212177+vuandy50@users.noreply.github.com>
2020-12-06 01:12:29 -0800

    Making orders

--- debug/moc_mainscreen.cpp
+++ debug/moc_mainscreen.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_mainScreen_t {
-    QByteArrayData data[1];
-    char stringdata0[11];
+    QByteArrayData data[15];
+    char stringdata0[260];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,30 @@ struct qt_meta_stringdata_mainScreen_t {
     )
 static const qt_meta_stringdata_mainScreen_t qt_meta_stringdata_mainScreen = {
     {
-QT_MOC_LITERAL(0, 0, 10) // "mainScreen"
+QT_MOC_LITERAL(0, 0, 10), // "mainScreen"
+QT_MOC_LITERAL(1, 11, 21), // "on_addBurger1_clicked"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 21), // "on_addBurger2_clicked"
+QT_MOC_LITERAL(4, 56, 21), // "on_addBurger3_clicked"
+QT_MOC_LITERAL(5, 78, 17), // "on_comboA_clicked"
+QT_MOC_LITERAL(6, 96, 17), // "on_comboB_clicked"
+QT_MOC_LITERAL(7, 114, 17), // "on_comboC_clicked"
+QT_MOC_LITERAL(8, 132, 19), // "on_addFries_clicked"
+QT_MOC_LITERAL(9, 152, 19), // "on_addChips_clicked"
+QT_MOC_LITERAL(10, 172, 16), // "on_cokeB_clicked"
+QT_MOC_LITERAL(11, 189, 18), // "on_spriteB_clicked"
+QT_MOC_LITERAL(12, 208, 17), // "on_waterB_clicked"
+QT_MOC_LITERAL(13, 226, 27), // "on_tabWidget_currentChanged"
+QT_MOC_LITERAL(14, 254, 5) // "index"
 
     },
-    "mainScreen"
+    "mainScreen\0on_addBurger1_clicked\0\0"
+    "on_addBurger2_clicked\0on_addBurger3_clicked\0"
+    "on_comboA_clicked\0on_comboB_clicked\0"
+    "on_comboC_clicked\0on_addFries_clicked\0"
+    "on_addChips_clicked\0on_cokeB_clicked\0"
+    "on_spriteB_clicked\0on_waterB_clicked\0"
+    "on_tabWidget_currentChanged\0index"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,22 +65,65 @@ static const uint qt_meta_data_mainScreen[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x08 /* Private */,
+       5,    0,   77,    2, 0x08 /* Private */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    0,   79,    2, 0x08 /* Private */,
+       8,    0,   80,    2, 0x08 /* Private */,
+       9,    0,   81,    2, 0x08 /* Private */,
+      10,    0,   82,    2, 0x08 /* Private */,
+      11,    0,   83,    2, 0x08 /* Private */,
+      12,    0,   84,    2, 0x08 /* Private */,
+      13,    1,   85,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   14,
+
        0        // eod
 };
 
 void mainScreen::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<mainScreen *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_addBurger1_clicked(); break;
+        case 1: _t->on_addBurger2_clicked(); break;
+        case 2: _t->on_addBurger3_clicked(); break;
+        case 3: _t->on_comboA_clicked(); break;
+        case 4: _t->on_comboB_clicked(); break;
+        case 5: _t->on_comboC_clicked(); break;
+        case 6: _t->on_addFries_clicked(); break;
+        case 7: _t->on_addChips_clicked(); break;
+        case 8: _t->on_cokeB_clicked(); break;
+        case 9: _t->on_spriteB_clicked(); break;
+        case 10: _t->on_waterB_clicked(); break;
+        case 11: _t->on_tabWidget_currentChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
 }
 
 QT_INIT_METAOBJECT const QMetaObject mainScreen::staticMetaObject = { {
@@ -89,6 +152,17 @@ void *mainScreen::qt_metacast(const char *_clname)
 int mainScreen::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 12)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 12;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 12)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 12;
+    }
     return _id;
 }
 QT_WARNING_POP
